c086832433cd64cb17e1b9fd7359f78b5dbacab0
mfadhili <mfadhili@github.com>
2023-12-21 17:29:00 +0300

    feat camp site#

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import Hero from "@/components/Hero";
+import Camp from "@/components/Camp";
+import Guide from "@/components/Guide";
+import Feature from "@/components/Feature";
+import GetApp from "@/components/GetApp";
 
 export default function Home() {
   return (
-      <h1 className="font-bold text-teal-500 text-2xl">
-        Hello World
-      </h1>
+       <>
+          <Hero/>
+          <Camp/>
+          <Guide/>
+          <Feature/>
+          <GetApp/>
+       </>
   )
 }
---
+++ components/Camp.tsx
@@ -0,0 +1,20 @@
+function Camp() {
+    return (
+        <section className={"2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20"}>
+            <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+                <CampSite/>
+            </div>
+        </section>
+    );
+}
+
+function CampSite() {
+    return (
+        <div></div>
+    );
+}
+
+
+
+
+export default Camp;
