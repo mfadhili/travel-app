b7e6d8e8109cf615d79bf13d3b871795c00f0f84
mfadhili <mfadhili@github.com>
2023-12-22 16:37:42 +0300

    feat footer complete  Changes to be committed: 	modified:   components/Footer.tsx 	modified:   constants/index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -62,7 +62,7 @@ export const FOOTER_LINKS = [
     },
     {
         title: 'Our Community',
-        links: ['Climbing xixixi', 'Hiking hilink', 'Hilink kinthill'],
+        links: ['Climbing', 'Hiking', 'Hilink hill'],
     },
 ];
 
